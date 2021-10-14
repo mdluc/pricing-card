ff2c0be28e466cf0517eb283b2978fe2272d9e74
mdluc <madalinaluceac@gmail.com>
2021-10-14 16:37:47 +0300

    - changed index

--- src/components/ProductFeatures/ProductFeatures.js
+++ src/components/ProductFeatures/ProductFeatures.js
@@ -2,14 +2,14 @@ import React from "react";
 import { FaCheck, FaTimes } from "react-icons/fa";
 
 
-const ProductFeatures = ({price,type, free, subscription}) =>{
+const ProductFeatures = ({id, price,type, free, subscription}) =>{
     return(
   <ul className="perks">
           <li>
             <FaCheck className="check-icon" />
             {free}
           </li>
-          {subscription.map((index,item) =>
+          {subscription.map((item, index) =>
             price > 0 ? (
               <li key={index}>
                 <FaCheck className="check-icon" />
