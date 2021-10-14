e49dcf13d73990490c3d21fb71de6c99f6c637f0
mdluc <madalinaluceac@gmail.com>
2021-10-14 16:16:17 +0300

    Updates

---
+++ precache-manifest.5b68a6b1e5b1a38241a98c4283fe89b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988f3a7f7eeeb05f92c755a8ebbf8094",
+    "url": "/pricing-card/index.html"
+  },
+  {
+    "revision": "928fea7a367fa861ae45",
+    "url": "/pricing-card/static/css/main.6dfd30ad.chunk.css"
+  },
+  {
+    "revision": "87a71e7628c6cdea8dd9",
+    "url": "/pricing-card/static/js/2.9ef09b31.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pricing-card/static/js/2.9ef09b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928fea7a367fa861ae45",
+    "url": "/pricing-card/static/js/main.5c2f86bc.chunk.js"
+  },
+  {
+    "revision": "74c1e3ccaf28efdbc9f1",
+    "url": "/pricing-card/static/js/runtime-main.616c4c4c.js"
+  }
+]);
